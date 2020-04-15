1be096e5f839ffca40c60ad9868506ffdb4599bd
nyo-kichi <nyo-kichi@users.noreply.github.com>
2020-04-15 11:20:34 +0900

    use ENV_VAR

--- travis/deploy-prd.sh
+++ travis/deploy-prd.sh
@@ -3,3 +3,5 @@
 echo 'deploy-prd.sh'
 echo TRAVIS_BRANCH: "$TRAVIS_BRANCH"
 echo TRAVIS_TAG: "$TRAVIS_TAG"
+
+echo ENV_VAR: "$ENV_VAR"
--- travis/deploy-stg.sh
+++ travis/deploy-stg.sh
@@ -3,3 +3,5 @@
 echo 'deploy-stg.sh'
 echo TRAVIS_BRANCH: "$TRAVIS_BRANCH"
 echo TRAVIS_TAG: "$TRAVIS_TAG"
+
+echo ENV_VAR: "$ENV_VAR"
